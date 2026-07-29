acce02f2fe12d4c111799873531a6290c6b0acca
Halil Taha Basegmez <tahabasegmez@gmail.com>
2026-07-29 12:28:32 +0300

    Revert "fix: carplay issues"
    This reverts commit 26f5f3ad5b47b4b8321001b0043aff4c9770ad0f.

--- ios/AACP/CarPlaySceneDelegate.m
+++ ios/AACP/CarPlaySceneDelegate.m
@@ -5,15 +5,13 @@
 
 /** Araç bağlandı: arayüz denetleyicisini react-native-carplay'e devret. */
 - (void)templateApplicationScene:(CPTemplateApplicationScene *)templateApplicationScene
-    didConnectInterfaceController:(CPInterfaceController *)interfaceController
-                          toWindow:(CPWindow *)window {
-  [RNCarPlay connectWithInterfaceController:interfaceController window:window];
+    didConnectInterfaceController:(CPInterfaceController *)interfaceController {
+  [RNCarPlay connectWithInterfaceController:interfaceController window:templateApplicationScene.carWindow];
 }
 
 /** Bağlantı koptu: JS tarafı da haberdar edilir ki şablonlar temizlensin. */
 - (void)templateApplicationScene:(CPTemplateApplicationScene *)templateApplicationScene
-    didDisconnectInterfaceController:(CPInterfaceController *)interfaceController
-                            fromWindow:(CPWindow *)window {
+    didDisconnectInterfaceController:(CPInterfaceController *)interfaceController {
   [RNCarPlay disconnect];
 }
 
